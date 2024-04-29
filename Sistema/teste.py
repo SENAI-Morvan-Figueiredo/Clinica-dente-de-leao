ec9980fcf2fe431fa26f3eae4a0e04461ffd081f
Marques103-23187257 <aluno103.23187257gabriel@gmail.com>
2024-04-29 19:28:03 -0300

    fazendo alguns testes para validação de datas nos models

--- Sistema/teste.py
+++ Sistema/teste.py
@@ -17,13 +17,15 @@ def is_feriado(value):
 def gera_horarios(dia):
     horarios = []
     
-    if not is_feriado(dia):
-        horarios = [(f'{i}', f'{(i + 8) + ':00'}' if len(i+8) < 2 else f'{'0'+(i + 8) + ':00'}') for i in range(1, 10)]
+    if is_feriado(dia) == True:
+        horarios = [(f'{i}', f'{i + 8}:00' if i+8 > 9 else f'0{i + 8}:00') for i in range(1, 10)]
         return horarios
     else:
-        horarios = [(f'{i}', f'{(i + 8) + ':00'}' if len(i+8) < 2 else f'{'0'+(i + 8) + ':00'}') for i in range(1, 4)]
+        horarios = [(f'{i}', f'{i + 8}:00' if i+8 > 9 else f'0{i + 8}:00') for i in range(1, 4)]
+        return horarios
 
 teste = date.today()
-print(teste)
+# print(teste)
 lista_horarios = gera_horarios(teste)
+print(lista_horarios)
 
