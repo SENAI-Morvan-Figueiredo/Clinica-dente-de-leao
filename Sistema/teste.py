fb69d50a98e44102b118a5b8f632e3c99d9c69ef
GabrielMarques02 <aluno103.23187257gabriel@gmail.com>
2024-04-29 22:00:05 -0300

    continuando testes para o models

--- Sistema/teste.py
+++ Sistema/teste.py
@@ -2,30 +2,29 @@ import holidays
 from datetime import date
 
 
-def is_feriado(value):
+def is_feriado(dia):
     objeto_feriados = holidays.country_holidays('BR')
     ano = f'{date.today()}'
-    lista_feriados = []
-    for objeto_feriado in objeto_feriados[f'{ano[:5]}-01-01' : f'{ano}-12-31']:
-        lista_feriados.append(objeto_feriado)   
+    # lista_feriados = []
+    for objeto_feriado in objeto_feriados[f'{ano[:5]}-01-01' : f'{ano[:5]}-12-31']:
 
-    if value in lista_feriados:
-        return True
-    else:
-        return False
+        if dia == objeto_feriado:
+            return True
+        else:
+            pass
     
 def gera_horarios(dia):
     horarios = []
     
-    if is_feriado(dia) == True:
-        horarios = [(f'{i}', f'{i + 8}:00' if i+8 > 9 else f'0{i + 8}:00') for i in range(1, 10)]
+    if not is_feriado(dia):
+        horarios = [(f'{i}', f'{i + 8}:{j}0' if i+8 > 9 else f'0{i + 8}:00') for i in range(1, 11) for j in range(0,4,3)]
         return horarios
     else:
-        horarios = [(f'{i}', f'{i + 8}:00' if i+8 > 9 else f'0{i + 8}:00') for i in range(1, 4)]
+        horarios = [(f'{i}', f'{i + 8}:00' if i+8 > 9 else f'0{i + 8}:00') for i in range(1, 5)]
         return horarios
-
+    
 teste = date.today()
-# print(teste)
+print(teste)
 lista_horarios = gera_horarios(teste)
 print(lista_horarios)
 
