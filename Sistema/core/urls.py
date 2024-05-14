6423820bf3e22e1e14a6ed4ece78fa2794bb0808
GabrielMarques02 <aluno103.23187257gabriel@gmail.com>
2024-05-13 21:52:32 -0300

    começando pagina de login

--- Sistema/core/urls.py
+++ Sistema/core/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls.conf import include
+from .views import IndexView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
 
-    path('funcionarios/', include('funcionarios.urls', namespace= 'funcionarios')),
-    path('clientes/', include('clientes.urls', namespace= 'clientes')),
-    path('contas/', include('contas.urls', namespace= 'contas')),
+    # path('funcionarios/', include('funcionarios.urls', namespace= 'funcionarios')),
+    # path('clientes/', include('clientes.urls', namespace= 'clientes')),
+    # path('contas/', include('contas.urls', namespace= 'contas')),
 ]
