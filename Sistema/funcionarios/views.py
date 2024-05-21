50fe08822818bc964de75802838bad2c39a1c387
Marques103-23187257 <aluno103.23187257gabriel@gmail.com>
2024-05-20 21:58:34 -0300

    atualizando: munando nome da pasta static/contas para static/accounts. começando: views funcionarios

--- Sistema/funcionarios/views.py
+++ Sistema/funcionarios/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-# Create your views here.
+class TestMixinIsAdmin(UserPassesTestMixin):
+    def test_func(self):
+        is_admin_or_is_staff = self.request.user.is_superuser or \
+            self.request.user.is_staff
+        return bool(is_admin_or_is_staff)
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request, "Você não tem permissões!"
+        )
+        return redirect("accounts:index")
+
 
