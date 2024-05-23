5942a0d04a7a075993bf6543cba784073d3c5eea
Marques103-23187257 <aluno103.23187257gabriel@gmail.com>
2024-05-22 21:10:24 -0300

    ajuste: formulario

--- Sistema/clientes/forms.py
+++ Sistema/clientes/forms.py
@@ -2,5 +2,6 @@ from django import forms
 from . import models
 
 class ClienteViewForm(forms.Form):
-    model = models.Cliente
-    fields=['cpf', 'genero', 'telefone', 'convenio', 'plano', 'cep', 'rua', 'numero', 'complemento', 'municipio', 'unidade_federal', 'data_nacimento']
+    class Meta:
+        model = models.Cliente
+        fields=['cpf', 'genero', 'telefone', 'convenio', 'plano', 'cep', 'rua', 'numero', 'complemento', 'municipio', 'unidade_federal', 'data_nacimento']
