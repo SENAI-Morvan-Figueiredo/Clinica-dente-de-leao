bf1d10c04489966c26ed5994dcc14ecea83e4565
Marques103-23187257 <aluno103.23187257gabriel@gmail.com>
2024-05-27 21:21:07 -0300

    adicionado: urls para cadatro de convenios e planos

--- Sistema/clientes/urls.py
+++ Sistema/clientes/urls.py
@@ -4,8 +4,12 @@ from . import views
 app_name = 'clientes'
 
 urlpatterns = [
-    path('registro/', views.cliente_cadastro, name='cliente_cadastro'),
+    path('registrar/dados-complementares/', views.cliente_cadastro, name='cliente_cadastro'),
     path('atualizar/', views.cliente_atualizar, name='cliente_atualizar'),
+    
+    path('cadatro/convenio/', views.cliente_atualizar, name='convenio_cadatro'),
+    path('cadatro/convenio/plano', views.cliente_atualizar, name='convenio_cadatro'),
+    
     # path('consultas/', views.consulta_lista, name='consulta_list'),
     # path('consultas/criar/', views.consulta_cadastro, name='consulta_create'),
     # path('consultas/editar/<int:pk>/', views.consulta_atualizar, name='consulta_update'),
