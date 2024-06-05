90dd8e329c4b63c3cc95e7ee6be1784c9760bc29
Marques103-23187257 <aluno103.23187257gabriel@gmail.com>
2024-06-05 18:47:03 -0300

    adicionado: novas funçoes de lista e cadastro

--- Sistema/funcionarios/froms.py
+++ Sistema/funcionarios/froms.py
@@ -5,8 +5,7 @@ class FuncionarioCreateForm(forms.Form):
     
     class Meta:
         model = models.Funcionario
-        fields =  ['nome', 'sobrenome','cpf', 'genero', 'telefone', 'cep', 'rua', 'numero', 'complemento', 'municipio', 'unidade_federal', 'data_nacimento', 'data_contratacao', 'is_medico'
-    ]
+        fields =  ['nome', 'sobrenome','cpf', 'genero', 'telefone', 'cep', 'rua', 'numero', 'complemento', 'municipio', 'unidade_federal', 'data_nacimento', 'data_contratacao', 'is_medico']
         
 class MedicoCreateForm(forms.Form):
     
--- Sistema/funcionarios/views.py
+++ Sistema/funcionarios/views.py
@@ -24,7 +24,9 @@ class FuncionarioCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
     model = Funcionario
     login_url = 'accounts:login'
     template_name = 'funcionarios/cadastro.html'
-    form_class = FuncionarioCreateForm
+    # form_class = FuncionarioCreateForm
+    fields =  ['nome', 'sobrenome','cpf', 'genero', 'telefone', 'cep', 'rua', 'numero', 'complemento', 'municipio', 'unidade_federal', 'data_nacimento', 'data_contratacao', 'is_medico']
+
     
     if Funcionario.is_medico == 1:
         success_url = reverse_lazy('funcionarios:medico_cadastro') 
@@ -45,32 +47,33 @@ class MedicoCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
     model = Medico
     login_url = 'accounts:login'
     template_name = 'funcionarios/cadastro.html'
-    form_class = MedicoCreateForm
+    # form_class = MedicoCreateForm
+    fields = ['crm', 'funcionario', 'especialidade']
     success_url = reverse_lazy('funcionarios:medicos_lista')
     
 class MedicoListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
     
     login_url = 'accounts:login'
-    template_name = 'funcionarios/medicos_list.html'
+    template_name = 'funcionarios/medico_lista.html'
 
     def get_queryset(self):
         return Medico.objects.all().order_by('-pk')
     
-# class EspecialidadeCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
+class EspecialidadeCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
 
-#     model = Especialidade
-#     login_url = 'accounts:login'
-#     template_name = 'funcionarios/cadastro.html'
-#     fields = ['nome',]
-#     success_url = reverse_lazy('funcionarios:especialidade_lista')
+    model = Especialidade
+    login_url = 'accounts:login'
+    template_name = 'funcionarios/cadastro.html'
+    fields = ['especialidade','descricao']
+    success_url = reverse_lazy('funcionarios:especialidade_lista')
     
-# class EspecialidadeListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
+class EspecialidadeListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
     
-#     login_url = 'accounts:login'
-#     template_name = 'funcionarios/especialidade_list.html'
+    login_url = 'accounts:login'
+    template_name = 'funcionarios/especialidade_lista.html'
 
-#     def get_queryset(self):
-#         return Especialidade.objects.all().order_by('-pk')
+    def get_queryset(self):
+        return Especialidade.objects.all().order_by('-pk')
 
 
 class AgendaCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
@@ -85,26 +88,26 @@ class AgendaCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
     
-# class AgendaUpdateView(LoginRequiredMixin, TestMixinIsAdmin, UpdateView):
+class AgendaUpdateView(LoginRequiredMixin, TestMixinIsAdmin, UpdateView):
 
-#     model = Agenda
-#     login_url = 'accounts:login'
-#     template_name = 'medicos/agenda_cadastro.html'
-#     fields = ['medico', 'dia', 'horario']
-#     success_url = reverse_lazy('funcionarios:agenda_lista')
+    model = Agenda
+    login_url = 'accounts:login'
+    template_name = 'medicos/agenda_cadastro.html'
+    fields = ['medico', 'dia', 'horario']
+    success_url = reverse_lazy('funcionarios:agenda_lista')
     
-#     def form_valid(self, form):
-#         form.instance.user = self.request.user
-#         return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
     
-# class AgendaDeleteView(LoginRequiredMixin, TestMixinIsAdmin, DeleteView):
-#     model = Agenda
-#     success_url = reverse_lazy('medicos:agenda_lista')
-#     template_name = 'form_delete.html'
+class AgendaDeleteView(LoginRequiredMixin, TestMixinIsAdmin, DeleteView):
+    model = Agenda
+    success_url = reverse_lazy('medicos:agenda_lista')
+    template_name = 'form_delete.html'
 
-#     def get_success_url(self):
-#         messages.success(self.request, "Consulta excluída com sucesso!")
-#         return reverse_lazy('medicos:agenda_lista')
+    def get_success_url(self):
+        messages.success(self.request, "Consulta excluída com sucesso!")
+        return reverse_lazy('medicos:agenda_lista')
 
 
 class AgendaListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
@@ -121,13 +124,13 @@ funcionario_cadastro = FuncionarioCreateView.as_view()
 funionarios_lista = FuncionarioListView.as_view()
 
 medico_cadastro = MedicoCreateView.as_view()
-# medico_lista = MedicoListView.as_view()
+medico_lista = MedicoListView.as_view()
 
-# especialidade_cadastro = EspecialidadeCreateView.as_view()
-# especialidade_lista = EspecialidadeListView.as_view()
+especialidade_cadastro = EspecialidadeCreateView.as_view()
+especialidade_lista = EspecialidadeListView.as_view()
 
 agenda_cadastro = AgendaCreateView.as_view()
-# agenda_atualizar = AgendaUpdateView.as_view()
+agenda_atualizar = AgendaUpdateView.as_view()
 agenda_lista = AgendaListView.as_view()
-# agenda_deletar = AgendaDeleteView.as_view()
+agenda_deletar = AgendaDeleteView.as_view()
 
